a6b0cd938d74438f85ef2a03a2db93d50d46b79a
RezaRiyaldi <52364173+RezaRiyaldi@users.noreply.github.com>
2021-01-14 17:11:13 +0700

    Update uas.java

--- Descending/src/uas.java
+++ Descending/src/uas.java
@@ -1,131 +1,132 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 public class uas {
     public static void garis() {
         System.out.println("\n============================");
     }
+    // Main
     public static void main(String [] args) {
         garis();
         System.out.println("Program No. 1");
-        // no1();
+        no1();
         garis();
         System.out.println("Program No. 2");
-        // no2();
+        no2();
         garis();
         System.out.println("Program No. 3");
-        // no3();
+        no3();
         garis();
         System.out.println("Program No. 4");
-        // no4();
+        no4();
         garis();
         System.out.println("Program No. 5");
         no5();
     }
-    // public static void no1() {
-    //     Scanner input = new Scanner(System.in);        
-    //     int n = 5;
+    public static void no1() {
+        Scanner input = new Scanner(System.in);        
+        int n = 5;
 
-    //     //deklarasi array
-    //     String kata[] = new String[n];
+        //deklarasi array
+        String kata[] = new String[n];
         
-    //     //input data ke array
-    //     for (int i=0; i<kata.length; i++){
-    //         System.out.print("Kata ke - " + (i + 1) + " : ");
-    //         kata[i] = input.next();
-    //     }
+        //input data ke array
+        for (int i=0; i<kata.length; i++){
+            System.out.print("Kata ke - " + (i + 1) + " : ");
+            kata[i] = input.next();
+        }
         
-    //     //menampilkan data dalam array
-    //     System.out.println("\nKata-kata yang dimasukan : ");
-    //     for (int i=0; i<kata.length; i++){
-    //         System.out.println(kata[i]);
-    //     }
-    // }
+        //menampilkan data dalam array
+        System.out.println("\nKata-kata yang dimasukan : ");
+        for (int i=0; i<kata.length; i++){
+            System.out.println(kata[i]);
+        }
+    }
 
-    // public static void no2() {
-    //     Scanner inputan = new Scanner(System.in);
-    //     int A[][] = new int[2][3];
-    //     int B[][] = new int[2][3];
-    //     int C[][] = new int[2][3];
-    //     System.out.println("[baris][kolom]");
-    //     System.out.println("Masukan nilai Matriks A : ");
-    //     System.out.println("-----------------------------");
-    //     for(int i=0; i<2; i++){
-    //         for(int j=0; j<3; j++){
-    //             System.out.print("[" +(i+1)+ "][" +(j+1)+ "]: ");
-    //             A[i][j] = inputan.nextInt();
-    //         }
-    //     }
+    public static void no2() {
+        Scanner inputan = new Scanner(System.in);
+        int A[][] = new int[2][3];
+        int B[][] = new int[2][3];
+        int C[][] = new int[2][3];
+        System.out.println("[baris][kolom]");
+        System.out.println("Masukan nilai Matriks A : ");
+        System.out.println("-----------------------------");
+        for(int i=0; i<2; i++){
+            for(int j=0; j<3; j++){
+                System.out.print("[" +(i+1)+ "][" +(j+1)+ "]: ");
+                A[i][j] = inputan.nextInt();
+            }
+        }
         
-    //     System.out.println("Masukan nilai Matriks B : ");
-    //     System.out.println("-----------------------------");
-    //     for(int i=0; i<2; i++){
-    //         for(int j=0; j<3; j++){
-    //             System.out.print("[" +(i+1)+ "][" +(j+1)+ "]: ");
-    //             B[i][j] = inputan.nextInt();
-    //         }
-    //     }
+        System.out.println("Masukan nilai Matriks B : ");
+        System.out.println("-----------------------------");
+        for(int i=0; i<2; i++){
+            for(int j=0; j<3; j++){
+                System.out.print("[" +(i+1)+ "][" +(j+1)+ "]: ");
+                B[i][j] = inputan.nextInt();
+            }
+        }
         
-    //     //penjumlahan matriks
-    //     for(int i=0; i<2; i++){
-    //         for(int j=0; j<3; j++){
-    //             C[i][j] = A[i][j] + B[i][j];
-    //         }
-    //     }
+        //penjumlahan matriks
+        for(int i=0; i<2; i++){
+            for(int j=0; j<3; j++){
+                C[i][j] = A[i][j] + B[i][j];
+            }
+        }
         
-    //     System.out.println("Hasil penjumlahan matriks : ");
-    //     System.out.println("-----------------------------");
-    //     for(int i=0; i<2; i++){
-    //         for(int j=0; j<3; j++){
-    //             System.out.print(+(C[i][j])+" ");
-    //         }
-    //         System.out.println(" ");
-    //     }
-    // }
+        System.out.println("Hasil penjumlahan matriks : ");
+        System.out.println("-----------------------------");
+        for(int i=0; i<2; i++){
+            for(int j=0; j<3; j++){
+                System.out.print(+(C[i][j])+" ");
+            }
+            System.out.println(" ");
+        }
+    }
 
-    // public static void no3(){
-    //     int angka[] = {3, 6, 1, 4, 2};
-    //     int temp;
-    //     System.out.println("Data sebelum diurut:");
-    //     for (int i = 0; i < angka.length; i++){
-    //         System.out.print(angka[i] + " ");
-    //     }
-    //     System.out.println();
+    public static void no3(){
+        int angka[] = {3, 6, 1, 4, 2};
+        int temp;
+        System.out.println("Data sebelum diurut:");
+        for (int i = 0; i < angka.length; i++){
+            System.out.print(angka[i] + " ");
+        }
+        System.out.println();
 
-    //     for (int i = angka.length - 1; i > 0; i--) {
-    //         for (int j = 0; j < i; j++) {
-    //             if (angka[i] > angka[j]) {
-    //                 temp = angka[i];
-    //                 angka[i] = angka[j];
-    //                 angka[j] = temp;
-    //             }
-    //         }
-    //     }
+        for (int i = angka.length - 1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (angka[i] > angka[j]) {
+                    temp = angka[i];
+                    angka[i] = angka[j];
+                    angka[j] = temp;
+                }
+            }
+        }
 
-    //     System.out.println("\nData setelah diurut:");
-    //     for (int i = 0; i < angka.length; i++) {
-    //         System.out.print(angka[i] + " ");
-    //     }
+        System.out.println("\nData setelah diurut:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.print(angka[i] + " ");
+        }
 
-    //     System.out.println("");
-    // }
+        System.out.println("");
+    }
 
-    // public static void no4(){
-    //     int input = 100;
-    //     System.out.println("Bilangan Prima 1 - 100 = ");
-    //     for(int i=2; i<input; i++){
-    //         boolean isPrima = true;
-    //         for (int j=2; j<i; j++){
-    //             if(i % j == 0){
-    //                 isPrima = false;
-    //                 break;
-    //             }
-    //         }
+    public static void no4(){
+        int input = 100;
+        System.out.println("Bilangan Prima 1 - 100 = ");
+        for(int i=2; i<input; i++){
+            boolean isPrima = true;
+            for (int j=2; j<i; j++){
+                if(i % j == 0){
+                    isPrima = false;
+                    break;
+                }
+            }
             
-    //         if(isPrima == true){
-    //             System.out.print(i);
-    //             System.out.print(", ");
-    //         }
-    //     }
-    // }
+            if(isPrima == true){
+                System.out.print(i);
+                System.out.print(", ");
+            }
+        }
+    }
     public static void no5(){
         int a,x,y;      
         a = 5;
